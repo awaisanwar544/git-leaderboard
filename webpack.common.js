667ca663a30a23cd1eae77f8c53556168a3929e6
awaisanwar544 <awaisanwar544@gmail.com>
2022-03-03 20:28:47 +0500

    setup project

---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+  },
+  // loaders
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(svg|ico|png|jpg|jpeg|webp|gif)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  // plugins
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Todo List',
+    filename: 'index.html',
+    template: path.resolve(__dirname, 'src/template.html'),
+  })],
+};
